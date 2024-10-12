1caf951380ff1a6c168473574889652cc19f03ab
mohammed dhedy <mhmddhedy1234@gmail.com>
2024-10-12 23:20:06 +0300

    complete the design

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+// ------------------ GENERAL BEHAVIOURS START ------------------
+let upBtn = document.querySelector(".upBtn");
+window.addEventListener("scroll",()=>{
+  if(window.scrollY>=200){
+    upBtn.classList.add("active");
+  }
+  else{
+    upBtn.classList.remove("active");
+  }
+})
+// ------------------------
+let as = document.querySelectorAll(".projects a");
+as.forEach((a) => {
+  a.setAttribute("target", "_blank");
+});
+// ------------------------
+let scrollLine=document.querySelector(".scrollLine");
+window.addEventListener("scroll",_=>{
+  let height=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+  let scrolled=document.documentElement.scrollTop;
+  scrollLine.style.width=`${(scrolled/height)*100}%`;
+})
+
+// ------------------ GENERAL BEHAVIOURS END ------------------
 let nav = document.querySelector("header .container nav");
 let menuIcon = document.querySelector("header .container nav .navIcon");
 menuIcon.onclick = () => {
@@ -23,54 +47,64 @@ let skillsMenuItem = Array.from(
 let skillsBoxes = Array.from(
   document.querySelectorAll(".skills .container .content .box ")
 );
-skillsMenuItem.forEach((skillCat)=>{
-skillCat.onclick=()=>{
+skillsMenuItem.forEach((skillCat) => {
+  skillCat.onclick = () => {
     removeActiveFromSkills();
     skillCat.classList.add("active");
-    let cat=skillCat.getAttribute("data-cat")
-    skillsBoxes.forEach((skillBox)=>{
-        if(cat=="all"){
-            skillBox.classList.add("active");
-        }else if(skillBox.dataset.type==cat){
-            skillBox.classList.add("active");
-        }
-    })
-}
-})
-let removeActiveFromSkills=()=>{
-    skillsMenuItem.forEach((skillCat)=>{
-        skillCat.classList.remove('active');
-    })
-    skillsBoxes.forEach((skillBox)=>{
-        skillBox.classList.remove("active");
-    })
-}
-let progressspans=Array.from(document.querySelectorAll(".skills .container .content .box .BackSide .progress span"));
-progressspans.forEach((spans)=>{
-  spans.style.width=`${spans.dataset.target}`;
-  spans.innerHTML=spans.dataset.target;
-})
-// contact 
-let textAreaInput=document.querySelector(".contact .content form .message textarea");
-let charCounter=document.querySelector(".contact .content form .message .Charlimit");
-let charProgressBar=document.querySelector(".contact .content form .message .CharProgress");
-let inputMaxLength=textAreaInput.getAttribute('maxlength');
-let submitBtn=document.querySelector('.contact .submitBtn')
-charCounter.innerHTML=inputMaxLength;
-textAreaInput.addEventListener('input',()=>{
-    let CurrentInputLength=textAreaInput.value.length;
-    charCounter.innerHTML=inputMaxLength-CurrentInputLength;
-    charProgressBar.style.width=`${(CurrentInputLength/inputMaxLength)*100}%`;
-    if(charCounter.innerHTML==0){
-        charCounter.classList.add('zero');
-        charProgressBar.style.backgroundColor='red';
-        submitBtn.setAttribute('title',"you have reached the limit :(");
-    }
-    else{
-        charCounter.classList.remove('zero');
-        charProgressBar.style.backgroundColor='var(--';
-        submitBtn.style.cursor="pointer";
-        submitBtn.setAttribute('title',"");
-    }
-})
-
+    let cat = skillCat.getAttribute("data-cat");
+    skillsBoxes.forEach((skillBox) => {
+      if (cat == "all") {
+        skillBox.classList.add("active");
+      } else if (skillBox.dataset.type == cat) {
+        skillBox.classList.add("active");
+      }
+    });
+  };
+});
+let removeActiveFromSkills = () => {
+  skillsMenuItem.forEach((skillCat) => {
+    skillCat.classList.remove("active");
+  });
+  skillsBoxes.forEach((skillBox) => {
+    skillBox.classList.remove("active");
+  });
+};
+let progressspans = Array.from(
+  document.querySelectorAll(
+    ".skills .container .content .box .BackSide .progress span"
+  )
+);
+progressspans.forEach((spans) => {
+  spans.style.width = `${spans.dataset.target}`;
+  spans.innerHTML = spans.dataset.target;
+});
+// contact
+let textAreaInput = document.querySelector(
+  ".contact .content form .message textarea"
+);
+let charCounter = document.querySelector(
+  ".contact .content form .message .Charlimit"
+);
+let charProgressBar = document.querySelector(
+  ".contact .content form .message .CharProgress"
+);
+let inputMaxLength = textAreaInput.getAttribute("maxlength");
+let submitBtn = document.querySelector(".contact .submitBtn");
+charCounter.innerHTML = inputMaxLength;
+textAreaInput.addEventListener("input", () => {
+  let CurrentInputLength = textAreaInput.value.length;
+  charCounter.innerHTML = inputMaxLength - CurrentInputLength;
+  charProgressBar.style.width = `${
+    (CurrentInputLength / inputMaxLength) * 100
+  }%`;
+  if (charCounter.innerHTML == 0) {
+    charCounter.classList.add("zero");
+    charProgressBar.style.backgroundColor = "red";
+    submitBtn.setAttribute("title", "you have reached the limit :(");
+  } else {
+    charCounter.classList.remove("zero");
+    charProgressBar.style.backgroundColor = "var(--titles-color)";
+    submitBtn.style.cursor = "pointer";
+    submitBtn.setAttribute("title", "submit message");
+  }
+});
